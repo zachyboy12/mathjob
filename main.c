a9eeb96efb24a87a8ca95f38a4be56fb05eb057c
Tumo <104463761+zachyboy12@users.noreply.github.com>
2022-07-15 11:55:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-    FILE *f;
-    char c;
-    f=fopen("df.csv","r");
-    while((c=fgetc(f))!=EOF){
-        printf("%c",c);
-    }
-    fclose(f);
-    return 0;
-}
+
